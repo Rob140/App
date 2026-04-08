d421e75c75a137e29229300c1073ad419e4dd059
Robiel <fisehahaileab@gmail.com>
2026-04-08 18:13:11 +0200

    useMemo

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import "./Product.css";
 
 const Product = () => {
+  // useMemo for expensive calculation
+  const memHandler = useMemo(() => {}, [dependency, clicked]);
+
+  const callbackHandler = useCallback(() => {}, []);
   return <div>Product</div>;
 };
 
